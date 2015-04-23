57a24c1a3466bb6d5529477fc2d162836e6b40b9
João Pinto <lamego.pinto@gmail.com>
2015-04-23 20:15:51 +0100

    Code cleanup

--- src/PlayBoard.py
+++ src/PlayBoard.py
@@ -21,7 +21,7 @@ class PlayBoard(cocos.layer.ColorLayer):
 
     def __init__(self):
         super(PlayBoard, self).__init__(64, 64, 224, 0)
-        self.playTable = {}         # Dict emulated sparse matrix, key: x,y tuple, value : tile_type
+        self.tile_grid = {}         # Dict emulated sparse matrix, key: tuple(x,y), value : tile_type
         self.imploding_tiles = []   # List of tile sprites being imploded, used during IMPLODING_TILES
         self.dropping_tiles = []    # List of tile sprites being dropped, used during DROPPING_TILES
         self.swap_start_pos = None  # Position of the first tile clicked for swapping
@@ -31,9 +31,9 @@ class PlayBoard(cocos.layer.ColorLayer):
         self.game_state = WAITING_PLAYER_MOVEMENT
 
     def swap_elements(self, elem1_pos, elem2_pos):
-        tile_type, sprite = self.playTable[elem1_pos]
-        self.playTable[elem1_pos] = self.playTable[elem2_pos]
-        self.playTable[elem2_pos] = tile_type, sprite
+        tile_type, sprite = self.tile_grid[elem1_pos]
+        self.tile_grid[elem1_pos] = self.tile_grid[elem2_pos]
+        self.tile_grid[elem2_pos] = tile_type, sprite
 
     def tile_sprite(self, tile_type, pos):
         """
@@ -41,35 +41,34 @@ class PlayBoard(cocos.layer.ColorLayer):
         :param pos: sprite position
         :return: sprite built form tile_type
         """
-        sprite = Sprite( tile_type)
+        sprite = Sprite(tile_type)
         sprite.position = pos
         sprite.scale = 1
         return sprite
 
     def fill_with_random_tiles(self):
         """
-        Fills the playTable with random tiles
+        Fills the tile_grid with random tiles
         """
-
-        playtable = {}
+        tile_grid = {}
         # Fill the data matrix with random tile types
         while True:  # Loop until we have a valid table (no imploding lines)
             for x in range(COLS_COUNT):
                 for y in range(ROWS_COUNT):
                     tile_type, sprite = choice(self.available_tiles), None
-                    playtable[x, y] = tile_type, sprite
-            if len(self.get_same_type_lines(playtable)) == 0:
+                    tile_grid[x, y] = tile_type, sprite
+            if len(self.get_same_type_lines(tile_grid)) == 0:
                 break
-            playtable = {}
+            tile_grid = {}
 
         # Build the sprites based on the assigned tile type
-        for key, value in playtable.iteritems():
+        for key, value in tile_grid.iteritems():
             tile_type, sprite = value
             sprite = self.tile_sprite(tile_type, self.to_display(key))
-            playtable[key] = tile_type, sprite
+            tile_grid[key] = tile_type, sprite
             self.add(sprite)
 
-        self.playTable = playtable
+        self.tile_grid = tile_grid
 
     def on_mouse_press(self, x, y, buttons, modifiers):
         if self.game_state == WAITING_PLAYER_MOVEMENT:
@@ -90,31 +89,30 @@ class PlayBoard(cocos.layer.ColorLayer):
             return
 
         # Start swap animation for both objects
-        tile_type, sprite = self.playTable[self.swap_start_pos]
+        tile_type, sprite = self.tile_grid[self.swap_start_pos]
         sprite.do(MoveTo(self.to_display(self.swap_end_pos), 0.4))
-        tile_type, sprite = self.playTable[self.swap_end_pos]
+        tile_type, sprite = self.tile_grid[self.swap_end_pos]
         sprite.do(MoveTo(self.to_display(self.swap_start_pos), 0.4) + CallFunc(self.on_tiles_swap_completed))
 
         # Swap elements at the board data grid
         self.swap_elements(self.swap_start_pos, self.swap_end_pos)
-
         self.game_state = SWAPPING_TILES
 
     def on_tiles_swap_completed(self):
         self.game_state = DROPPING_TILES
 
         if len(self.implode_lines()) == 0:
-            # No lines impleted, roll back game play
-            self.game_state = SWAPPING_TILES
+            # No lines imploded, roll back the game play
 
             # Start swap animation for both objects
-            tile_type, sprite = self.playTable[self.swap_start_pos]
+            tile_type, sprite = self.tile_grid[self.swap_start_pos]
             sprite.do(MoveTo(self.to_display(self.swap_end_pos), 0.4))
-            tile_type, sprite = self.playTable[self.swap_end_pos]
+            tile_type, sprite = self.tile_grid[self.swap_end_pos]
             sprite.do(MoveTo(self.to_display(self.swap_start_pos), 0.4) + CallFunc(self.on_tiles_swap_back_completed))
 
             # Revert on the grid
             self.swap_elements(self.swap_start_pos,  self.swap_end_pos)
+            self.game_state = SWAPPING_TILES
 
     def on_tiles_swap_back_completed(self):
          self.game_state = WAITING_PLAYER_MOVEMENT
@@ -127,7 +125,7 @@ class PlayBoard(cocos.layer.ColorLayer):
         """
         return CELL_WIDTH/2 + row * CELL_WIDTH, CELL_HEIGHT/2 + col * CELL_HEIGHT
 
-    def get_same_type_lines(self, playtable, min_count=3):
+    def get_same_type_lines(self, tile_grid, min_count=3):
         """
         Identify vertical and horizontal lines composed of min_count consecutive elements
         :param min_count: minimum consecutive elements to identify a line
@@ -139,7 +137,7 @@ class PlayBoard(cocos.layer.ColorLayer):
             same_type_list = []
             last_tile_type = None
             for y in range(ROWS_COUNT):
-                tile_type, sprite = playtable[x, y]
+                tile_type, sprite = tile_grid[x, y]
                 if last_tile_type == tile_type:
                     same_type_list.append((x, y))
                 if tile_type != last_tile_type or y == ROWS_COUNT-1:  # Line end because type changed or edge reached
@@ -153,7 +151,7 @@ class PlayBoard(cocos.layer.ColorLayer):
             same_type_list = []
             last_tile_type = None
             for x in range(COLS_COUNT):
-                tile_type, sprite = playtable[x, y]
+                tile_type, sprite = tile_grid[x, y]
                 if last_tile_type == tile_type:
                     same_type_list.append((x, y))
                 if tile_type != last_tile_type or x == COLS_COUNT-1:  # Line end because of type change or edge reached
@@ -170,9 +168,9 @@ class PlayBoard(cocos.layer.ColorLayer):
         """
         :return: Implodes lines with more than 3 elements of the same type
         """
-        for x, y in self.get_same_type_lines(self.playTable):
-            same_type, sprite = self.playTable[x, y]
-            self.playTable[x, y] = None
+        for x, y in self.get_same_type_lines(self.tile_grid):
+            same_type, sprite = self.tile_grid[x, y]
+            self.tile_grid[x, y] = None
             self.imploding_tiles.append(sprite)  # Track tiles being imploded
             sprite.do(ScaleTo(0, 0.5) | RotateTo(180, 0.5) + CallFuncS(self.on_tile_remove))  # Implode animation
         if len(self.imploding_tiles) > 0:
@@ -183,27 +181,27 @@ class PlayBoard(cocos.layer.ColorLayer):
 
     def drop_groundless_tiles(self):
         """
-        Walk on all columns, from boottom to up:
+        Walk on all columns, from bottom to up:
             a) count gap or move down pieces for gaps already counted
             b) on top drop as much tiles as gaps counted
         :return:
         """
-        playtable = self.playTable
+        tile_grid = self.tile_grid
 
         for x in range(COLS_COUNT):
             gap_count = 0
             for y in range(ROWS_COUNT):
-                if playtable[x, y] is None:
+                if tile_grid[x, y] is None:
                     gap_count += 1
                 elif gap_count > 0:  # Move from y to y-gap_count
-                    tile_type, sprite = playtable[x, y]
+                    tile_type, sprite = tile_grid[x, y]
                     if gap_count > 0:
                         sprite.do(MoveTo(self.to_display((x, y-gap_count)), 0.3*gap_count))
-                    playtable[x, y-gap_count] = tile_type, sprite
+                    tile_grid[x, y-gap_count] = tile_type, sprite
             for n in range(gap_count):  # Drop as much tiles as gaps counted
                 tile_type = choice(self.available_tiles)
                 sprite = self.tile_sprite(tile_type, self.to_display((x, y+n+1)))
-                playtable[x, y-gap_count+n+1] = tile_type, sprite
+                tile_grid[x, y-gap_count+n+1] = tile_type, sprite
                 sprite.do(
                     MoveTo(self.to_display((x, y-gap_count+n+1)), 0.3*gap_count) + CallFuncS(self.on_drop_completed))
                 self.add(sprite)
@@ -227,5 +225,5 @@ class PlayBoard(cocos.layer.ColorLayer):
         for y in range(ROWS_COUNT-1, -1, -1):
             line_str = ''
             for x in range(COLS_COUNT):
-                line_str += str(self.playTable[x, y][0])
+                line_str += str(self.tile_grid[x, y][0])
             print line_str
